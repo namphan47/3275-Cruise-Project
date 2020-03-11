33b9254082f73882997f33ca8281bd37b842148f
nam <nphan@atomiton.com>
2020-03-10 18:53:40 -0700

    update ui

---
+++ project/src/main/java/com/nameme/project/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,33 @@
+package com.nameme.project.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+//		return new Class[] { RootConfig.class };
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		servletContext.setInitParameter("spring.profiles.active", "h2");
+	}
+	
+}
