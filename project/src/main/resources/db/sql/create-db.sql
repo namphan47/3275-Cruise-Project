d44668795e62adc7f348ec65462c448a9d3cc7fd
Karan-95 <karan.singh.990@live.com>
2020-03-13 02:52:12 -0700

    DB Initial II

--- project/src/main/resources/db/sql/create-db.sql
+++ project/src/main/resources/db/sql/create-db.sql
@@ -1,7 +1,12 @@
 DROP TABLE cruise IF EXISTS;
-DROP TABLE registrations IF EXISTS;
-DROP TABLE students IF EXISTS;
-DROP TABLE courses IF EXISTS;
+DROP TABLE route IF EXISTS;
+DROP TABLE customer IF EXISTS;
+DROP TABLE room IF EXISTS;
+DROP TABLE roomType IF EXISTS;
+DROP TABLE booking IF EXISTS;
+DROP TABLE activity IF EXISTS;
+DROP TABLE onBoardActivity IF EXISTS;
+DROP TABLE portOfCall IF EXISTS;
 
 CREATE TABLE cruise (
 cruiseID INT PRIMARY KEY,
@@ -37,4 +42,40 @@ CREATE TABLE room(
   
 );
 
+CREATE TABLE roomType(
+  roomTypeID INTEGER PRIMARY KEY,
+  name VARCHAR(50),
+  basePrice NUMBER,
+  description VARCHAR(50)	
+);
+
+CREATE TABLE booking(
+ bookingID INTEGER PRIMARY KEY,
+ guestCount INTEGER,
+ customerID INTEGER,
+ routeID INTEGER,
+ activityID INTEGER,
+ roomNumber INTEGER,
+ FOREIGN KEY (roomNumber) references room(roomNumber),
+ FOREIGN KEY (activityID) references activity(activityID),
+ FOREIGN KEY (routeID) references route(routeID),
+ FOREIGN KEY (customerID) references customer(customerID) 
+
+);
+
+CREATE TABLE activity(
+ activityID INTEGER PRIMARY KEY,
+ activityName VARCHAR(50),
+ guestCount INTEGER,
+ description VARCHAR(50),
+ price NUMBER,
+ imageSrc VARCHAR(50)
+);
+
+CREATE TABLE portOfCall(
+ portName VARCHAR(50),
+ portDesc VARCHAR(50),
+ price NUMBER,
+ imageSrc VARCHAR(50)
+);
 
